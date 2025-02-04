28e4f9e2fb8bf16ea58ee2c77044ec6a6d8b2a0b
sagheerNaskSoft <sagheerahmad.nasksoft@gmail.com>
2025-02-04 20:55:43 +0500

    latest code

--- src/Component/Contactus/FormMap.js
+++ src/Component/Contactus/FormMap.js
@@ -119,11 +119,11 @@ function FormMap({snakbar}) {
                                 <div className='inp_box'>
                                     <label>Message <span className='text-danger'>*</span></label>
                                     <textarea value={data?.message} onChange={(e) => setData({ ...data, message: e.target.value })} type='text' placeholder='Message' className='w-100' />
-                                    {falidError?.message && <span className='error'>Please Enter Your message</span>}
+                                    {falidError?.message && <span className='error mt-1'>Please Enter Your message</span>}
                                 </div>
                             </div>
                             <div className='col-12 d-flex'>
-                                <button type='submit' className='banner_btn'>Submit</button>
+                                <button type='submit' className='banner_btn mt-2'>Submit</button>
                             </div>
                         </div>
                     </form>
